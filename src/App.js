21596312c5b0ac57b2fb8c6bef56e0dc1a9de2a9
thomenat <156375450+thomenat@users.noreply.github.com>
2024-08-11 18:46:04 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import golden from './golden.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Spell Wiz - spells </h1>
+      <img src={golden}  className="App-logo" alt="Golden" />
+    </div>
+  );
+}
+
+export default App;
