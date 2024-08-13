9a43fbd5a7eff8c81857ece203a6721dcc8ca3af
thomenat <156375450+thomenat@users.noreply.github.com>
2024-08-13 21:41:48 +0100

    added axios and API call

--- src/SpellDictionary.js
+++ src/SpellDictionary.js
@@ -1,13 +1,24 @@
 import React, { useState} from "react";
+import axios from "axios";
 import "./SpellDictionary.css";
 
 export default function SpellDictionary() {
    let [keyword, setKeyword] = useState(" ");
 
+   function handleResponse(response) {
+    console.log(response.data);
+}
+
    function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
 }
+
+
+let spellSlug = keyword
+let apiUrl = `https://api.potterdb.com/v1/spells/${spellSlug}`
+
+axios.get(apiUrl).then(handleResponse);
 
    function handleKeywordChange(event) {
     setKeyword(event.target.value);
